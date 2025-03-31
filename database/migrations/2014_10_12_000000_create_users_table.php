b2069ec135332e0e5f91e36e2b479b21e04f8485
vocaothuyhuyen <114867276+vocaothuyhuyen@users.noreply.github.com>
2025-03-31 21:44:55 +0700

    cap nhat phan mêm ios 18.1.1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('phone');
+            $table->string('address');
+            $table->string('gender1');
+            $table->string('gender2');
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
